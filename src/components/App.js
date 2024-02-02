77c3a86db5660deaa4443d94b313e8277a2a8cd3
hyunju0824 <hyunjuhj0824@gmail.com>
2024-02-03 00:59:40 +0900

    로그인

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '../App.css';
 import { Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
       <RecoilRoot>
         <Routes>
-          <Route path='/' element={<></>} />
+          <Route path='/' element={<Login></Login>} />
         </Routes>
       </RecoilRoot>
     </div>
