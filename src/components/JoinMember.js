1a55cbc1547b6304339990d5d8c96cd220d4311a
hyunju0824 <hyunjuhj0824@gmail.com>
2024-02-05 11:53:08 +0900

    회원가입 atom, recoilPersist

--- src/components/JoinMember.js
+++ src/components/JoinMember.js
@@ -61,6 +61,21 @@ export default function JoinMember() {
                             />
                         </div>
                     </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="profileImage" className="block text-sm font-semibold leading-6 text-gray-900">
+                            프로필 사진
+                        </label>
+                        <div className="mt-2.5">
+                            <input
+                                type="file"
+                                accept="image/*"
+                                name="profileImage"
+                                id="profileImage"
+                                autoComplete="organization"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
                 </div>
                 <div className="mt-10">
                     <button
--- src/recoil/atoms/atom.js
+++ src/recoil/atoms/atom.js
@@ -1,6 +1,23 @@
 import { atom } from "recoil";
 
+// 로컬 저장
+import { recoilPersist } from "recoil-persist";
+const { persistAtom } = recoilPersist();
+
+
+// 로그인
 export const loginState = atom ({
     key: 'loginState',
     default: false,
+    effects_UNSTABLE: [persistAtom],
+});
+
+// 회원가입
+export const joinMemberState = atom({
+    key: 'joinMemberState',
+    default: {
+        email: '',
+        password: '',
+        profileImage: null,
+    }
 });
